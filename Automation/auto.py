cf9f271d71a7255ee8b21f79e1481969d41c7075
David Jaffe <david.ezekial.jaffe@gmail.com>
2023-03-09 10:01:50 -0500

    modify to generate reminder email text, subject, address. add csv from B2MMS for email addresses

--- Automation/auto.py
+++ Automation/auto.py
@@ -13,6 +13,7 @@ import copy
 #import glob # used in __init__
 import ConfigParser
 import subprocess
+import readB2MMS
 
 
 class auto():
@@ -21,6 +22,33 @@ class auto():
         self.debug = debug
         print 'auto.__init__ debug',self.debug
 
+        #### for generating reminder email
+        self.readB2MMS = readB2MMS.readB2MMS()
+
+        self.B2Members = self.readB2MMS.readB2M()
+        if debug > 1 : print self.B2Members
+        for B2id in self.B2Members:
+            fn,ln,email,category,inst = self.B2Members[B2id]
+            if debug > 1 : print 'self.B2Members[B2id]',B2id,self.B2Members[B2id],fn,ln,email,category,inst
+            if int(B2id)%100==0:
+                if debug > 0 : print '{:}, {:} {:}'.format(ln,fn,email)
+
+                    
+
+
+        self.reminderTemplate =     "Dear {colleague},\n\n \
+    Thank you for your presentation at the {conference} on behalf of the Belle II collaboration.\n\n \
+    If you plan to submit a paper for the conference Proceedings (which might be required or optional, depending on the conference), please notify {responsiblePCmember} {rPCemail} of the Belle II Publications Committee (PC). The PC is responsible for conference Proceedings submitted on behalf of the Belle II Collaboration.\n\n \
+    Further instructions can be found in the PC Review of Conference Proceedings at {link} .\n\n \
+    Best regards,\n \
+    {rSCmember} on behalf of the Speakers Committee"
+        self.responsiblePCmember = 'Alan Schwartz'
+        self.responsibleSCmember  = 'David Jaffe'
+        self.PClink = 'https://confluence.desy.de/display/BI/Review*of*conference*proceedings'
+        self.PClink = 'https://confluence.desy.de/x/zhuyCw'
+
+
+
         self.get_dest = '../Talks/ToDo/'
 
         self.fav_items = {'talk_url': ["curl","URL","-o","DEST"], 
@@ -28,7 +56,8 @@ class auto():
                               'title':1,
                               'author':2,
                               'abstract':3,
-                              'date':4
+                              'date':4,
+                              'conference':5,
                               }
         m = -1
         for k in self.fav_items:
@@ -37,7 +66,7 @@ class auto():
         if m>-1:
             self.newRecord_template = range(m+1)
         return
-    def first(self,seckey=None,DryRun=True):
+    def first(self,seckey=None,DryRun=True,Reminder=False):
         ''' 
         first try at using configparser 
         read in all sections from config file
@@ -112,6 +141,9 @@ class auto():
                             else:
                                 newRecord[self.fav_items[fav]] = [fav,v]
                 if self.debug>0: print 'auto.first new record for',sec
+                conferenceName = seckey
+                for pair in newRecord:
+                    if pair[0]=='conference' : conferenceName = pair[1]
                 for pair in newRecord:
                     secondPart = pair[1]
                     if pair[1]=='':
@@ -119,21 +151,65 @@ class auto():
                         if sec not in errorLog: errorLog[sec] = []
                         errorLog[sec].append( pair )
                     if self.debug>0 or 'ERROR' in secondPart: print pair[0],secondPart
+                    if Reminder and pair[0]=='author':
+                        self.composeReminder(secondPart,conferenceName)
         print 'auto.first Processed',nEntries,'entries'
                                 
         for key in errorLog:
             print 'auto.first ERROR',key,errorLog[key]
         return
+    def composeReminder(self,author,conference):
+        '''
+        compose reminder email for author and conference
+        '''
+        subject = 'Conference proceedings reminder'
+        email = self.getEmail(author)
+        if email is None : email = 'NO EMAIL FOUND FOR THIS SPEAKER'
+        rPCemail = self.getEmail(self.responsiblePCmember)
+        reminder = self.reminderTemplate.format(colleague=author, \
+                    conference=conference,responsiblePCmember=self.responsiblePCmember, \
+                    rPCemail=rPCemail,rSCmember=self.responsibleSCmember,link=self.PClink)
+        print '\n',email,'\n', subject, '\n', reminder, '\n'
+        return
+    def getEmail(self,author):
+        '''
+        return email address given author name
+    
+        B2Members[B2id] =[firstname,middlename,lastname,lastnameprefix,inst]
+        B2MemberEmail[B2id] = email
+
+        '''
+        s = author.split()
+        firstn = s[0]
+        lastn = s[-1]
+        middlen = None
+        if len(s)>2 : mn = s[1]
+        matches = []
+        for B2id in self.B2Members:
+            fn,ln,email,category,inst = self.B2Members[B2id]
+            if ln.lower()==lastn.lower() :
+                if fn.lower()==firstn.lower() :
+                    matches.append(B2id)
+
+        if self.debug > 0 :
+            print 'auto.getEmail author',author,'# matches',len(matches)
+            for B2id in matches: print self.B2Members[B2id][2]
+        email = None
+        if len(matches) > 0 : email = self.B2Members[matches[0]][2]
+        return email
+    
+                    
 if __name__ == '__main__' :
 
     seckey = "DoesNotMatch"  # required argument
-    debug = 0
     DryRun = True    # do not curl remote file, this is only a dry run
+    debug = 0
+    Reminder = False # print conference proceedings reminder message
     #print 'sys.argv',sys.argv
     if len(sys.argv)>1:
         seckey = sys.argv[1]
     else:
-        sys.exit('python auto.py CONFERENCE_SHORT_NAME [DryRun(T/F)] [debug]')
+        sys.exit('python auto.py CONFERENCE_SHORT_NAME [DryRun([T]/F)] [debug] [Reminder([F]/T)]')
     if len(sys.argv)>2:
         a = sys.argv[2]
         if a.upper()=='T':
@@ -144,6 +220,14 @@ if __name__ == '__main__' :
             DryRun = bool(sys.argv[2])
     if len(sys.argv)>3:
         debug = int(sys.argv[3])
+    if len(sys.argv)>4:
+        a = sys.argv[4]
+        if a.upper()=='T' :
+            Reminder = True
+        elif a.upper()=='F' :
+            Reminder = False
+        else:
+            Reminder = bool(a)
             
     A = auto(debug=debug)
-    A.first(seckey=seckey,DryRun=DryRun)
+    A.first(seckey=seckey,DryRun=DryRun,Reminder=Reminder)
